388dada382e411393f541e1300aae1a34f829aa4
Drew Crawford <drew@sealedabstract.com>
2022-09-05 15:41:52 -0500

    NSFontDescriptorAttributeName

--- src/nsfontdescriptor.rs
+++ src/nsfontdescriptor.rs
@@ -3,6 +3,39 @@ use objr::bindings::*;
 use coretextr::CTFontDescriptor;
 use coregraphicsr::{CGFloat};
 
+objc_class_newtype! {
+    pub struct NSFontDescriptorAttributeName: NSString;
+}
+extern "C" {
+    static NSFontFamilyAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontNameAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontFaceAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontSizeAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontVisibleNameAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontMatrixAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontVariationAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontCharacterSetAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontCascadeListAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontTraitsAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontFixedAdvanceAttribute: &'static NSFontDescriptorAttributeName;
+    static NSFontFeatureSettingsAttribute: &'static NSFontDescriptorAttributeName;
+}
+
+impl NSFontDescriptorAttributeName {
+    #[inline] pub fn family() -> &'static Self { unsafe { &NSFontFamilyAttribute } }
+    #[inline] pub fn name() -> &'static Self { unsafe { &NSFontNameAttribute } }
+    #[inline] pub fn face() -> &'static Self { unsafe { &NSFontFaceAttribute } }
+    #[inline] pub fn size() -> &'static Self { unsafe { &NSFontSizeAttribute } }
+    #[inline] pub fn visible_name() -> &'static Self { unsafe { &NSFontVisibleNameAttribute } }
+    #[inline] pub fn matrix() -> &'static Self { unsafe { &NSFontMatrixAttribute } }
+    #[inline] pub fn variation() -> &'static Self { unsafe { &NSFontVariationAttribute } }
+    #[inline] pub fn character_set() -> &'static Self { unsafe { &NSFontCharacterSetAttribute } }
+    #[inline] pub fn cascade_list() -> &'static Self { unsafe { &NSFontCascadeListAttribute } }
+    #[inline] pub fn traits() -> &'static Self { unsafe { &NSFontTraitsAttribute } }
+    #[inline] pub fn fixed_advance() -> &'static Self { unsafe { &NSFontFixedAdvanceAttribute } }
+    #[inline] pub fn feature_settings() -> &'static Self { unsafe { &NSFontFeatureSettingsAttribute } }
+}
+
 #[cfg(feature="bridge")]
 use core_foundationr::CTFontDescriptor;
 objc_class! {
